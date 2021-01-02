325218a0aa42ed418dd91e65a368cd7899cc488e
AI Guru <guruprasadmuppana@gmail.com>
2021-01-02 15:06:30 +0530

    Revert "Update BasicShapes.py"
    This reverts commit 10a1c777affc0a6a36faf19c0db9c14c4f6254ae.

--- ImageOperations/Blur.py
+++ ImageOperations/Blur.py
@@ -1,92 +1,100 @@
-
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
-#Salt and Pepper noise
-def saltandpepper(img,snr):
-    h=img.shape[0]
-    w=img.shape[1]
-    imgDest=img.copy()
-    sp=h*w
-    NP=int(sp*(1-snr))
-    for i in range (NP):
-        randx=np.random.randint(1,h-1)   # random
-        randy=np.random.randint(1,w-1)
-        if np.random.random()<=0.5:
-            imgDest[randx,randy]=0
-        else:
-            imgDest[randx,randy]=255
-    return imgDest
-
-
-file = "Images/Sunflower.jpg"
-
+file = "Images/hair.jpg"
 
 # Reading the image
 img = cv2.imread(file)
-
 # Displaying the image
 cv2.imshow("original",img)
 
-# 3x3 filter
-blur_filter3x3 = np.ones((3, 3), np.float) / (9.0)
-print(blur_filter3x3)
+# blur = cv2.blur(img, (7,7))
+# cv2.imshow('Blur', blur)
+# #
+# imgGaussianBlur = cv2.GaussianBlur(img,(7,7),0) # odd numbers for kernel
+# cv2.imshow("GaussianBlur",imgGaussianBlur)
+#
+# median = cv2.medianBlur(img, 7)
+# cv2.imshow('Median Blurring', median)
+#
+# bilateral = cv2.bilateralFilter(img, 5, 75, 75) # 95,95
+# cv2.imshow('Bilateral Blurring', bilateral)
 
-filter = np.array( [
-    [-1,-1,-1],
-    [-1,20,-1],
-    [-1,-1,-1]
-])
-
-# filter = np.array( [
-#     [1,1,1],
-#     [1,15,1],
-#     [1,1,1]
-# ])
-blur_filterCustom = filter/ (16.0)
-print(blur_filterCustom)
-
+#
+# # Custom filers
+# # 3x3 filter
+# blur_filter3x3 = np.ones((3, 3), np.float) / (9.0)
 # # 5x5 filter
 # blur_filter5x5 = np.ones((5, 5), np.float) / (25.0)
 # # 7x7 filter
 # blur_filter7x7 = np.ones((7, 7), np.float) / (49.0)
-
-image_blur3x3 = cv2.filter2D(img, -1, blur_filterCustom)
+# #
+# image_blur3x3 = cv2.filter2D(img, -1, blur_filter3x3)
 # image_blur5x5 = cv2.filter2D(img, -1, blur_filter5x5)
 # image_blur7x7 = cv2.filter2D(img, -1, blur_filter7x7)
-
+#
 # cv2.imshow('blur3x3', image_blur3x3)
 # cv2.imshow('blur5x5', image_blur5x5)
 # cv2.imshow('blur7x7', image_blur7x7)
 
-
-blur = cv2.blur(img, (3,3))
-cv2.imshow('Blur', blur)
-
-imgGaussianBlur = cv2.GaussianBlur(img,(7,7),0) # odd numbers for kernel
-cv2.imshow("GaussianBlur",imgGaussianBlur)
-
-
-imgslat = saltandpepper(img, 0.8)
-cv2.imshow("salt and pepper",imgslat)
-
-median = cv2.medianBlur(img, 5)
-cv2.imshow('Median Blurring', median)
-
-bilateral = cv2.bilateralFilter(img, 9, 95, 95)
-cv2.imshow('Bilateral Blurring', bilateral)
+#
+# blur_filter5x5 = np.ones((5, 5), np.float) / (25.0)
+# image_blur5x5 = cv2.filter2D(img, -1, blur_filter5x5)
+# blur = cv2.blur(img, (5,5))
+# cv2.imshow('blur from CV2', blur)
+# cv2.imshow('custom filter ', image_blur5x5)
 
 
-merged_img = cv2.addWeighted(img, 0.5,image_blur3x3, 0.5, 0)
-cv2.imshow('merged_img', merged_img)
+#Sharpening
+sharpenignfilter = np.array( [
+    [-1,-1,-1],
+    [-1,9,-1],
+    [-1,-1,-1]
+])
+image_sharpening = cv2.filter2D(img, -1, sharpenignfilter)
+cv2.imshow('Sharpening', image_sharpening)
 
+
+
+# non-local means of de-noising . non-local means filter takes into account of all pixel of the image
+# instead of local (kernel) area alone.
+imgNonLocalMeans = cv2.fastNlMeansDenoisingColored(img, None, 6,6,7,21)
+imgNonLocalMeans = cv2.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 15)
+# imgNonLocalMeans = cv2.fastNlMeansDenoisingColored(img, None, 20, 20, 7, 15)
+cv2.imshow("Non-local means of denosing", imgNonLocalMeans)
+
+# with gray color images.
+imgGray= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgNonLocalMeansGray = cv2.fastNlMeansDenoising(imgGray, None, 10, 10)
+cv2.imshow("gray non-local",imgNonLocalMeansGray)
 
 
 # Wait until any key is pressed
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+
+
+
+
+
+
+
+# file = "Images/Sunflower.jpg"
+# file = "Images/Sunflower-medium.jpg"
+
+
+#
+#
+# filter = np.array( [
+#     [1,1,1],
+#     [1,15,1],
+#     [1,1,1]
+# ])
+# blur_filterCustom = filter/ (23.0)
+# image_custom = cv2.filter2D(img, -1, blur_filterCustom)
+# cv2.imshow('Custom', image_custom)
+#
 
 
 def mid(img):
